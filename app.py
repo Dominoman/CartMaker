78654cab764a42a6dc1cb22df4a2e277adcb6988
Dominoman <olaca@freemail.hu>
2024-01-18 10:11:01 +0100

    Memory Widget first push

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ import sys
 from PySide6.QtCore import QItemSelection
 from PySide6.QtWidgets import QMainWindow, QApplication, QFileDialog, QMessageBox
 
-from crt import Crt, CrtException, EasyFS
+from crt import Crt, CrtException, EasyFS, EasyFile
 from mainwindow import Ui_MainWindow
+from widgets import MemoryWidget
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
@@ -14,13 +15,20 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.actionE_xit.triggered.connect(self.file_exit)
         self.actionOpen_crt.triggered.connect(self.file_open)
         self.actionExport.triggered.connect(self.file_export)
+        self.actionAdd_apps.triggered.connect(self.file_add)
+        self.actionDelete_apps.triggered.connect(self.file_delete)
         self.model = EasyFS(self)
         self.tableView.setModel(self.model)
         selection_model = self.tableView.selectionModel()
         selection_model.selectionChanged.connect(self.sel_changed)
 
+        self.widget = MemoryWidget(self.centralwidget)
+        self.widget.setModel(self.model)
+        self.verticalLayout.addWidget(self.widget)
+
     def sel_changed(self, selected: QItemSelection, deselected: QItemSelection) -> None:
         self.actionExport.setEnabled(selected.count() > 0)
+        self.actionDelete_apps.setEnabled(selected.count() > 0)
 
     def file_exit(self) -> None:
         self.close()
@@ -44,7 +52,17 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             for index in indexes:
                 file = self.model.files[index.row()]
                 file.export(path)
-            QMessageBox.information(self,"Export",f"Exported {len(indexes)} file(s).")
+            QMessageBox.information(self, "Export", f"Exported {len(indexes)} file(s).")
+
+    def file_add(self) -> None:
+        path = QFileDialog.getOpenFileNames(self, "Add files", filter="PRG files (*.prg);;All files (*.*)")
+        if len(path[0]) != 0:
+            for file in path[0]:
+                ef = EasyFile.load_file(file)
+                self.model.add_file(ef)
+
+    def file_delete(self) -> None:
+        pass
 
 
 if __name__ == "__main__":
